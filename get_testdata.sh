045f15c8557a7fa66abab4a73bb250944cd929f1
Chris Kerr <debdepba@dasganma.tk>
2014-11-06 08:25:44 +0000

    Script to download the test data from figshare

---
+++ get_testdata.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+## Test data are posted on FigShare, listed in this article
+# http://figshare.com/articles/galvani_test_data/1228760
+
+cd tests/testdata
+
+/usr/bin/wget -i - <<END_FILELIST
+http://files.figshare.com/1778905/arbin1.res
+http://files.figshare.com/1778937/bio_logic2.mpt
+http://files.figshare.com/1778938/bio_logic5.mpt
+http://files.figshare.com/1778939/bio_logic1.mpr
+http://files.figshare.com/1778940/bio_logic6.mpr
+http://files.figshare.com/1778941/bio_logic4.mpt
+http://files.figshare.com/1778942/bio_logic5.mpr
+http://files.figshare.com/1778943/bio_logic2.mpr
+http://files.figshare.com/1778944/bio_logic6.mpt
+http://files.figshare.com/1778945/bio_logic1.mpt
+http://files.figshare.com/1778946/bio_logic3.mpr
+http://files.figshare.com/1780444/bio_logic4.mpr
+END_FILELIST
