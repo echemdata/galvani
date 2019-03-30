7a5887fb381c9e960aaaa7c754d6082e22b28dab
Chris Kerr <chris.kerr@mykolab.ch>
2019-03-30 15:31:18 +0100

    Update existing get_testdata links to HTTPS version

--- get_testdata.sh
+++ get_testdata.sh
@@ -7,20 +7,20 @@ mkdir -p tests/testdata
 cd tests/testdata
 
 /usr/bin/wget --continue -i - <<END_FILELIST
-http://files.figshare.com/1778905/arbin1.res
-http://files.figshare.com/1778937/bio_logic2.mpt
-http://files.figshare.com/1778938/bio_logic5.mpt
-http://files.figshare.com/1778939/bio_logic1.mpr
-http://files.figshare.com/1778940/bio_logic6.mpr
-http://files.figshare.com/1778941/bio_logic4.mpt
-http://files.figshare.com/1778942/bio_logic5.mpr
-http://files.figshare.com/1778943/bio_logic2.mpr
-http://files.figshare.com/1778944/bio_logic6.mpt
-http://files.figshare.com/1778945/bio_logic1.mpt
-http://files.figshare.com/1778946/bio_logic3.mpr
-http://files.figshare.com/1780444/bio_logic4.mpr
-http://files.figshare.com/1780529/121_CA_455nm_6V_30min_C01.mpr
-http://files.figshare.com/1780530/121_CA_455nm_6V_30min_C01.mpt
-http://files.figshare.com/1780526/CV_C01.mpr
-http://files.figshare.com/1780527/CV_C01.mpt
+https://files.figshare.com/1778905/arbin1.res
+https://files.figshare.com/1778937/bio_logic2.mpt
+https://files.figshare.com/1778938/bio_logic5.mpt
+https://files.figshare.com/1778939/bio_logic1.mpr
+https://files.figshare.com/1778940/bio_logic6.mpr
+https://files.figshare.com/1778941/bio_logic4.mpt
+https://files.figshare.com/1778942/bio_logic5.mpr
+https://files.figshare.com/1778943/bio_logic2.mpr
+https://files.figshare.com/1778944/bio_logic6.mpt
+https://files.figshare.com/1778945/bio_logic1.mpt
+https://files.figshare.com/1778946/bio_logic3.mpr
+https://files.figshare.com/1780444/bio_logic4.mpr
+https://files.figshare.com/1780529/121_CA_455nm_6V_30min_C01.mpr
+https://files.figshare.com/1780530/121_CA_455nm_6V_30min_C01.mpt
+https://files.figshare.com/1780526/CV_C01.mpr
+https://files.figshare.com/1780527/CV_C01.mpt
 END_FILELIST
